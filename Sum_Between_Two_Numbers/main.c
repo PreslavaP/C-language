49155e47d0af6eb2a930170e122fe1209ba0a189
Preslava-Petrova <preslavap20@gmail.com>
2025-02-26 22:52:28 +0200

    Sum_Between_Two_Numbers done!

--- Sum_Between_Two_Numbers/main.c
+++ Sum_Between_Two_Numbers/main.c
@@ -3,6 +3,29 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int num1, num2;
+    printf("Enter num1: ");
+    scanf("%d", &num1);
+    printf("Enter num2: ");
+    scanf("%d", &num2);
+
+    int sum = 0;
+    int start = 0;
+    int end = 0;
+
+    if (num1 < num2) {
+        start = num1;
+        end = num2;
+    } else {
+        start = num2;
+        end = num1;
+    }
+
+    for(int i= start + 1; i < end; ++i)
+    {
+        sum += i;
+    }
+
+    printf("%d", sum);
     return 0;
 }
